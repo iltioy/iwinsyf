0f928d2bdc61cbf425667b6fe22fa9368e69f124
iltioy <tema.illar2@gmail.com>
2023-06-01 12:23:27 +0300

    init niche ne rabotaet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import useSound from "use-sound";
+import halsey from "./audio/halsey.mp3";
+import styles from "./App.module.css";
+import Draggable from "react-draggable";
+import { useEffect, useState, useRef } from "react";
+
+const audio = new Audio(halsey);
+audio.currentTime = 0.5;
+
+let time = 0;
+let lineWidth = 300;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [timeLine, setTimeLine] = useState(0);
+    const [bound, setBound] = useState(0);
+
+    const dotRef = useRef();
+
+    const play = () => {
+        // audio.currentTime += 5;
+        audio.play();
+    };
+
+    const stop = () => {
+        audio.pause();
+        time = audio.currentTime;
+    };
+
+    const handleStop = (e, data) => {
+        audio.currentTime = (data.x / lineWidth) * audio.duration;
+    };
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (!audio.paused && audio.currentTime < audio.duration - 5) {
+                setTimeLine(
+                    (prevState) => prevState + lineWidth / audio.duration
+                );
+                setBound((prevState) => prevState - timeLine);
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [audio.paused, audio.currentTime, audio.duration]);
+
+    return (
+        <div className={styles.main}>
+            <button onClick={() => play()}>Click</button>
+            <button onClick={() => stop()}>Stop</button>
+            <br />
+            <div className={styles.line}>
+                <Draggable
+                    axis="x"
+                    bounds={{ left: bound, right: lineWidth - 8.5 + bound }}
+                    onStop={handleStop}
+                >
+                    <div
+                        ref={dotRef}
+                        className={styles.dot}
+                        style={{ left: timeLine, top: -8.5 }}
+                    />
+                </Draggable>
+            </div>
+        </div>
+    );
 }
 
 export default App;
